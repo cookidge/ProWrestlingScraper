895c4a753cbf7809c1e79c6f4c903516fef64180
James Cooke <james-cooke@live.co.uk>
2019-03-31 15:49:26 +0100

    loops through a to z, and each page contained within

--- ProWrestlingScraper/ProWrestlingScraper.py
+++ ProWrestlingScraper/ProWrestlingScraper.py
@@ -5,19 +5,52 @@
 __author__      = "James Cooke"
 __copyright__   = "Copyright 2019"
 
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
 # site http://www.profightdb.com
 # wrestlers a to z /atoz.html?term=a&start=0
 # term runs a through to z, start is wrestlers on the page, increments of 100
 
-# loop through each term
+domain = "http://www.profightdb.com/atoz.html"
+alpha = "abcdefghijklmnopqrstuvwxyz"
 
-	# loop through each start (page)
+# loop through each page
+for index in range(len(alpha)):
+    page = "?term=" + alpha[index] # retrieve page
+    params = page + "&start="
+    currentUrl = domain + params # build page url
 
-		# loop through each wrestler
+    # retrieve page HTML
+    response = requests.get(currentUrl)                     # retrieve page html
+    soup = BeautifulSoup(response.text, "html.parser")
+    lasttag = soup.find_all("a",text = "last", limit = 1)   # retrieve href from last page <a> tag
 
-			# store data (name etc) in row, and retrieve url
+    if len(lasttag) > 0:
+        lastlink = lasttag[0].get('href')
+        last = lastlink.replace(params, "")
+        itr = int(last) / 100                               # calculate number of pages
+    else:
+        itr = 0
 
-				# access wrestler url - scrape data on wrestler
+    start = 0                                               # default start
+
+
+    # loop through number of pages
+    for cur in range(int(itr)+1):
+        print(currentUrl + str(start))                      # set page for [a,b,c] number [1,2,3] i.e. term=a&start=700                
+        start = start + 100
+
+time.sleep(1)
+
+
+	    # loop through each wrestler
+
+    		# store data (name etc) in row, and retrieve url
+
+	        # access wrestler url - scrape data on wrestler
 
 
 

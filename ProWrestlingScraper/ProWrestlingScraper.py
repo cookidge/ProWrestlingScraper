f323a9f1f04c8802410975ed1158f1f68ee516cf
James Cooke <james-cooke@live.co.uk>
2019-03-31 00:05:49 +0000

    added comment

--- ProWrestlingScraper/ProWrestlingScraper.py
+++ ProWrestlingScraper/ProWrestlingScraper.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
+
+"""ProWrrestlingScraper.py: Prototype scraper of Pro-Wrestling data from www.profightdb.com"""
+
+__author__      = "James Cooke"
+__copyright__   = "Copyright 2019"
+
+# site http://www.profightdb.com
+# wrestlers a to z /atoz.html?term=a&start=0
+# term runs a through to z, start is wrestlers on the page, increments of 100
+
+# loop through each term
+
+	# loop through each start (page)
+
+		# loop through each wrestler
+
+			# store data (name etc) in row, and retrieve url
+
+				# access wrestler url - scrape data on wrestler
+
+
+
 
